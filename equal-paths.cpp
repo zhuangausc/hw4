04ca79d27c48d8ed6c00b45d629faebca61c70d5
Angela Zhuang <zhuanga@usc.edu>
2025-04-07 07:02:50 +0000

    equal paths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,18 +1,37 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <algorithm>
 #endif
 
 #include "equal-paths.h"
 using namespace std;
 
-
 // You may add any prototypes of helper functions here
+int pathLength(Node* root){
+  if(root==NULL){
+    return 0;
+  }
+  return std::max(pathLength(root->left), pathLength(root->right)) + 1;
+}
 
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+  if(root==NULL){
+    return true;
+  }
+  else if(root->left==NULL && root->right==NULL){
+    return true;
+  }
+  else if(root->left==NULL){
+    return equalPaths(root->right);
+  }
+  else if(root->right==NULL){
+    return equalPaths(root->left);
+  }
+  else {
+    return pathLength(root->left) == pathLength(root->right) && equalPaths(root->left) && equalPaths(root->right);
+  }
 
 }
 
